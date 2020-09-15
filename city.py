76e00bb27216ec37f17535811fd1ec09ab857fb9
John LaSala <john@Johns-MacBook-Air.local>
2020-09-14 22:45:17 -0400

    add features

--- city.py
+++ city.py
@@ -21,19 +21,43 @@ class City:
         for names in res:
             cities.append(names['matching_full_name'])
 
+        if len(cities) == 0:
+            return "Error, no cities found"
+            
         return cities
+    
+    def details(self):
+        res = self._json['_embedded']['city:search-results']
+
+        if len(res) == 0:
+            return
+
+        res = res[0]['_links']['city:item']['href']
+        response = (requests.get(res)).json()
+
+        coord = response['location']['latlon']
+        population = 'Population: ' + str(response['population'])
+        country = 'Country: ' + response['_links']['city:country']['name']
+        timeZone = 'Time Zone: ' + response['_links']['city:timezone']['name'] + ' time'
+        coordinates = 'Coordinates: ' + str((coord['latitude'], coord['longitude']))
+
+        return [population, country, timeZone, coordinates]
+
+    def bundle(self):
+        data = [self.details(), self.cityList()]
+        return data
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def root():
     if request.method == 'POST':
-        city = request.form['userEmail']
+        city = request.form['information']
         newCity = City(str(city))
-        newCity = newCity.cityList()
-        return render_template('city.html', data=newCity)
+        data = newCity.bundle()
+        return render_template('city.html', data=data)
     else:
         return render_template('index.html')
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
